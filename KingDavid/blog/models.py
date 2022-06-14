f5ce7ea04fc7b84cbcf7fb078d9aec675e0a0204
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-06-14 12:22:31 +0100

    little adjustment to the Author,Created_date and Published_date models

--- KingDavid/blog/models.py
+++ KingDavid/blog/models.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth import get_user_model
 # Author = get_user_model
+
 # Create your models here.
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
-    # Author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    Created_date = models.DateField()
-    Published_date = models.DateField()
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Created_date = models.DateTimeField(default=datetime.now)
+    Published_date = models.DateTimeField(default=datetime.now)
