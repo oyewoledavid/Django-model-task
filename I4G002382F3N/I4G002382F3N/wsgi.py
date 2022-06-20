e9970844fe4601254c8b19f78a2dc407e18fb0e7
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-06-20 11:45:29 +0100

    change project name to my student id

--- I4G002382F3N/I4G002382F3N/wsgi.py
+++ I4G002382F3N/I4G002382F3N/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for KingDavid project.
+WSGI config for I4G002382F3N project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KingDavid.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G002382F3N.settings')
 
 application = get_wsgi_application()
